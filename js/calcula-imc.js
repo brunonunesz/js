797e629492e4bea378bd76cdb8d8be3fa505d4cf
brunonunesz <brunonunesz@outlook.com>
2019-01-11 15:33:38 -0200

    First commit

---
+++ js/calcula-imc.js
@@ -0,0 +1,63 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+	var paciente = pacientes[i];
+
+	 var tdPeso = paciente.querySelector(".info-peso");
+	 var tdAltura = paciente.querySelector(".info-altura");
+	 var tdImc = paciente.querySelector(".info-imc");
+
+	 var peso = tdPeso.textContent;
+	 var altura = tdAltura.textContent;
+
+	var ehValidoPeso = validaPeso(peso);
+	var ehValidoAltura = validaAltura(altura);
+
+	if(!ehValidPeso){
+		ehValidoPeso = false;
+		tdPeso.textContent = "Peso inválido";
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(!ehValidoAltura){
+		ehValidoAltura = false;
+		tdAltura.textContent = "Altura inválida";
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(ehValidoAltura && ehValidoPeso){
+		var imc = calculaImc(peso, altura);
+		tdImc.textContent = imc;
+	}
+
+}
+
+function calculaImc(peso, altura){
+	var imc = peso / (altura * altura);
+
+	return imc.toFixed(2);
+}
+
+function validaPeso(peso){
+	if(peso > 0 && peso <= 300){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function validaAltura(altura){
+	if(altura > 0 && altura <= 3.00){
+		return true;
+	} else{
+		return false;
+	}
+}
+
+ 
+
